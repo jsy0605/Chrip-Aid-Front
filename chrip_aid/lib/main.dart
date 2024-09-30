4f8d9ad0b45c10a6475e11bf167a95834c2584dc
jsy0605 <jungsy0605@naver.com>
2024-09-30 16:30:19 +0900

    [fix] firebase 초기화 코드 작성

--- chrip_aid/lib/main.dart
+++ chrip_aid/lib/main.dart
@@ -4,14 +4,23 @@ import 'package:chrip_aid/common/go_router/go_router.dart';
 import 'package:chrip_aid/common/google_map/google_map.dart';
 import 'package:chrip_aid/common/utils/aws_utils.dart';
 import 'package:chrip_aid/common/utils/snack_bar_util.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'firebase_options.dart'; // Firebase 옵션 파일을 가져옵니다.
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter 프레임워크 초기화
   await dotenv.load(fileName: ".env");
+
+  // Firebase 초기화 코드 추가
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await initFCM();
   await initGoogleMap();
   await initAuthority();
